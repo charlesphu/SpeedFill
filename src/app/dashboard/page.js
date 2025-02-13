6a0c177100a70d8cdc49d6ae19ae23cd4e5639d1
Dino <73320617+GreenDeno@users.noreply.github.com>
2025-02-13 10:42:29 -0800

    Add Panel component to Dashboard for displaying user information

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -4,12 +4,13 @@ import { Box, Typography } from "@mui/material";
 import Background from "../components/Background";
 import Title from "../components/Title";
 import Container from "../components/Container";
+import Panel from "../components/Panel";
 
 const Dashboard = () => {
   return (
     <>
       <Box
-        style={{
+        sx={{
           width: "50%",
           margin: "0 auto",
           paddingTop: "2rem",
@@ -19,10 +20,27 @@ const Dashboard = () => {
           tertiaryText="Upload your resume and let AI help you land your dream job!"
         />
       </Box>
+
       <Container
         title="Your Infomation"
         subtitle="Manage your password and saved resume"
-      />
+        sx={{ marginTop: "3rem", width: "60%" }}>
+        <Panel>
+          <Typography variant="h5" color="title">
+            Email Address:
+          </Typography>
+          <Typography variant="body1" color="title">
+            email@domain.com
+          </Typography>
+          <Typography variant="h5" color="title" marginTop="0.8rem">
+            Password:
+          </Typography>
+          <Typography variant="body1" color="title" marginBottom="-0.4rem">
+            **********
+          </Typography>
+        </Panel>
+      </Container>
+
       <Background imageUrl="/background.jpg" />
     </>
   );
