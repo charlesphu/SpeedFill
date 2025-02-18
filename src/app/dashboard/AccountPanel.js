377ce4599b7d5f090708d8fb56747b5f094efe53
Dino <73320617+GreenDeno@users.noreply.github.com>
2025-02-17 20:13:01 -0800

    Add AccountPanel functionality to update password

--- src/app/dashboard/AccountPanel.js
+++ src/app/dashboard/AccountPanel.js
@@ -1,5 +1,5 @@
-import { Box, Typography } from "@mui/material";
-import { useState } from "react";
+import { Box, Typography, TextField } from "@mui/material";
+import { useEffect, useRef, useState } from "react";
 
 import Container from "../components/Container";
 import Panel from "../components/Panel";
@@ -9,6 +9,31 @@ import Image from "next/image";
 const AccountPanel = ({ sx }) => {
   const [isShowingPassword, setShowPassword] = useState(false);
   const [isEditingPassword, setEditPassword] = useState(false);
+
+  const [password, setPassword] = useState("your_very_long_password");
+  const [shownPassword, setShownPassword] = useState("");
+
+  const passwordFieldRef = useRef();
+
+  useEffect(() => {
+    if (isShowingPassword) {
+      setShownPassword(password);
+    } else {
+      setShownPassword(password.replace(/./g, "•"));
+    }
+  }, [isShowingPassword, password]);
+
+  const captureEditPassword = () => {
+    passwordFieldRef.current.focus();
+    setShowPassword(true);
+    setEditPassword(true);
+  };
+
+  const releaseEditPassword = () => {
+    console.log("Updating password to", passwordFieldRef.current.value);
+    setEditPassword(false);
+    setShowPassword(false);
+  };
 
   return (
     <Container
@@ -36,16 +61,40 @@ const AccountPanel = ({ sx }) => {
             <Typography variant="h5" color="title">
               Password:
             </Typography>
-            <Typography
-              variant="body1"
-              color="title"
-              overflow="hidden"
-              textOverflow="ellipsis"
-              maxWidth="16rem">
-              {isShowingPassword
-                ? "your_very_long_password"
-                : "•••••••••••••••"}
-            </Typography>
+            <TextField
+              inputRef={passwordFieldRef}
+              variant="standard"
+              value={shownPassword}
+              onChange={(e) => {
+                if (isEditingPassword) setPassword(e.target.value);
+              }}
+              onBlur={releaseEditPassword}
+              sx={{
+                maxWidth: "16rem",
+                overflow: "hidden",
+                textOverflow: "ellipsis",
+                fontSize: "1rem",
+
+                "& .MuiInputBase-root": {
+                  color: "text.main",
+                },
+                "& .MuiInput-underline:after": {
+                  borderBottomColor: isEditingPassword
+                    ? "text.main"
+                    : "transparent",
+                },
+                "& .MuiInput-underline:before": {
+                  borderBottomColor: isEditingPassword
+                    ? "accent.main"
+                    : "transparent",
+                },
+                "& .MuiInput-underline:hover:not(.Mui-disabled):before": {
+                  borderBottomColor: isEditingPassword
+                    ? "accent.main"
+                    : "transparent",
+                },
+              }}
+            />
           </Box>
           <Box display="flex" gap="0.8rem" marginBottom="-0.8rem">
             <ActionButton
@@ -54,7 +103,10 @@ const AccountPanel = ({ sx }) => {
               }
               onClick={() => setShowPassword(!isShowingPassword)}
             />
-            <ActionButton icon="/icons/Edit.svg" />
+            <ActionButton
+              icon="/icons/Edit.svg"
+              onClick={captureEditPassword}
+            />
           </Box>
         </Box>
       </Panel>
