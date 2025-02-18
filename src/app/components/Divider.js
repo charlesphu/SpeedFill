70d73038cc4faaeb3acef51fb852b3a34d359b93
Dino <73320617+GreenDeno@users.noreply.github.com>
2025-02-18 00:24:36 -0800

    Adjust Auth Page styling consistency

--- src/app/components/Divider.js
+++ src/app/components/Divider.js
@@ -1,26 +1,26 @@
-import { Divider } from '@mui/material';
+import { Divider } from "@mui/material";
 
 const CustomDivider = () => {
-    return (
-        <Divider sx={{
-            fontSize: '0.7rem',
-            width: "50%",
-            maxWidth: "500px",
-            fontWeight: 'thin',
-            color: "white",
-            margin: "0 auto",
-            '&::before, &::after': {
-                borderColor: "white",
-            },
-            '& span': {
-                fontWeight: "bold",
-                color: "white",
-                textAlign: "center",
-            }
-        }}>
-            OR
-        </Divider>
-    );
+  return (
+    <Divider
+      sx={{
+        fontSize: "0.9rem",
+        width: "100%",
+        fontWeight: "thin",
+        color: "white",
+        margin: "0 auto",
+        "&::before, &::after": {
+          borderColor: "white",
+        },
+        "& span": {
+          fontWeight: "bold",
+          color: "white",
+          textAlign: "center",
+        },
+      }}>
+      OR
+    </Divider>
+  );
 };
 
 export default CustomDivider;
