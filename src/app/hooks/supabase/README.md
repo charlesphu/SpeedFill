667c13b874fb1fb1d687877c3f7f45f1b1fe0b5f
charlesphu <charlesphu18@gmail.com>
2025-04-17 09:39:09 -0700

    added some supabase funtions, but need to restructure for context

---
+++ src/app/hooks/supabase/README.md
@@ -0,0 +1,3 @@
+We can use a context to store the userID instead of making too many calls
+fetch user ID when app loads, and store it
+update this auth when the user logs out
--- src/app/hooks/supabase/structuredData.js
+++ src/app/hooks/supabase/structuredData.js
@@ -6,6 +6,42 @@ import { supabase, getUserID } from "./auth";
 // allData: unique_id, time, filepath (what resume), type, response
 // entries: unique_id, time, jobURL, jobTitle, company, date, status
 
+// buckets: (file storage)
+// filesStorage: userid -> filename (timestamp-filename)
+
+async function getFileURL(file) {
+  const userId = await getUserID();
+  const { data } = supabase.storage.from("filesStorage").getPublicUrl(file); // change to createSignedUrl
+  return data;
+}
+
+// currentResume: userid -> filename (timestamp-filename)
+
+async function getCurrentResumeURL() {
+    const userId = await getUserID();
+    var bucketName = "currentResume";
+    var folderPath = userId;
+    const { data, error } = await supabase.storage
+      .from(bucketName)
+      .list(folderPath);
+  
+    if (!data || data.length === 0) {
+      return null;
+    }
+    if (error) {
+      console.error("Error listing files:", error);
+      return null;
+    }
+  
+    const firstFile = data[0];
+    const filePath = `${folderPath}/${firstFile.name}`;
+  
+    const publicUrl = supabase.storage.from(bucketName).getPublicUrl(filePath)
+      .data.publicUrl;
+  
+    return publicUrl;
+  }
+
 // Foreign key constraints ..
 // userData.unique_id references allData.unique_id references entries.unique_id
 
@@ -14,5 +50,7 @@ import { supabase, getUserID } from "./auth";
 // ie user just wants to keep track of what they done
 
 /* Entry upload and retrieval*/
-
+async function insertData() {}
 /* File upload and retrieval*/
+
+export { insertData };
